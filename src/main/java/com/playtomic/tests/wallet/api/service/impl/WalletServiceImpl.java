90b07b92f916ffe60ffb78ba0445ba42bc6bbce9
adamu abdulaziz <ajitata6f@gmail.com>
2022-01-01 22:45:58 +0100

    feat: add impl package in service package

---
+++ src/main/java/com/playtomic/tests/wallet/api/service/impl/WalletServiceImpl.java
@@ -0,0 +1,13 @@
+package com.playtomic.tests.wallet.api.service.impl;
+
+import com.playtomic.tests.wallet.api.dto.WalletDTO;
+import com.playtomic.tests.wallet.api.service.WalletService;
+
+public class WalletServiceImpl implements WalletService {
+
+    @Override
+    public WalletDTO getWallet(int id) {
+        return null;
+    }
+
+}
