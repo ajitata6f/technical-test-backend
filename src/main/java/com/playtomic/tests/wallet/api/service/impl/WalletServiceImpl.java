9b4cef29d452386d2e4f790399bb38f2310bbcb6
adamu abdulaziz <ajitata6f@gmail.com>
2022-01-02 00:57:41 +0100

    refactor: inject StripeService instance using constructor injection

--- src/main/java/com/playtomic/tests/wallet/api/service/impl/WalletServiceImpl.java
+++ src/main/java/com/playtomic/tests/wallet/api/service/impl/WalletServiceImpl.java
@@ -2,8 +2,17 @@ package com.playtomic.tests.wallet.api.service.impl;
 
 import com.playtomic.tests.wallet.api.dto.WalletDTO;
 import com.playtomic.tests.wallet.api.service.WalletService;
+import com.playtomic.tests.wallet.service.StripeService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class WalletServiceImpl implements WalletService {
+
+    private StripeService stripeService;
+
+    public WalletServiceImpl(StripeService stripeService) {
+        this.stripeService = stripeService;
+    }
 
     @Override
     public WalletDTO getWallet(int id) {
