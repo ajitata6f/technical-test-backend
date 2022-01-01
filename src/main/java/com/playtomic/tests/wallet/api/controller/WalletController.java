889e2d902684a374f8b86ae63654dfc319d7c703
adamu abdulaziz <ajitata6f@gmail.com>
2022-01-01 13:07:43 +0100

    refactor: move WalletController.java
    moved WalletController.java from src/main/java/com/playtomic/tests/wallet/api/ into r/WalletController.java

--- src/main/java/com/playtomic/tests/wallet/api/controller/WalletController.java
+++ src/main/java/com/playtomic/tests/wallet/api/controller/WalletController.java
@@ -2,15 +2,18 @@ package com.playtomic.tests.wallet.api;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/wallets")
 public class WalletController {
     private Logger log = LoggerFactory.getLogger(WalletController.class);
 
-    @RequestMapping("/")
+    @GetMapping("/")
     void log() {
         log.info("Logging from /");
     }
+
 }
