e2b08ea5dd977a4971ffad3ad82cab6bf086337b
adamu abdulaziz <ajitata6f@gmail.com>
2022-01-01 20:49:09 +0100

    feat: add WalletRepository.java

--- src/main/java/com/playtomic/tests/wallet/api/repository/WalletRepository.java
+++ src/main/java/com/playtomic/tests/wallet/api/repository/WalletRepository.java
@@ -3,6 +3,6 @@ package com.playtomic.tests.wallet.api.repository;
 import com.playtomic.tests.wallet.api.model.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface WalletRepository extends JpaRepository<Wallet, Long> {
+public interface WalletRepository extends JpaRepository<Wallet, Integer> {
 
 }
