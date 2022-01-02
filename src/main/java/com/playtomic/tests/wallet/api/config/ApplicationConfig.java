c36e97923f9ec93643d00ec131bfb5166cea6f56
adamu abdulaziz <ajitata6f@gmail.com>
2022-01-02 01:19:40 +0100

    feat: add ApplicationConfig class for beans configuration

---
+++ src/main/java/com/playtomic/tests/wallet/api/config/ApplicationConfig.java
@@ -0,0 +1,11 @@
+package com.playtomic.tests.wallet.api.config;
+
+import com.playtomic.tests.wallet.service.StripeService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfig {
+
+
+}
