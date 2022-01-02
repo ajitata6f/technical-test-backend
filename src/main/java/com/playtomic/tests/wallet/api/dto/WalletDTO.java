cea3d2276c53289ebaada88a8695aff1de9a1b85
adamu abdulaziz <ajitata6f@gmail.com>
2022-01-02 06:15:40 +0100

    refactor: add instance fields

--- src/main/java/com/playtomic/tests/wallet/api/dto/WalletDTO.java
+++ src/main/java/com/playtomic/tests/wallet/api/dto/WalletDTO.java
@@ -1,5 +1,17 @@
 package com.playtomic.tests.wallet.api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class WalletDTO {
+
+    private Integer id;
+    private BigDecimal balance;
 
 }
