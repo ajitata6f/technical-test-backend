67edc66d30ae2df978c0b25f4e591e74c59f5531
adamu abdulaziz <ajitata6f@gmail.com>
2022-01-02 06:50:36 +0100

    feat: add getWallet controller method

--- src/main/java/com/playtomic/tests/wallet/api/controller/WalletController.java
+++ src/main/java/com/playtomic/tests/wallet/api/controller/WalletController.java
@@ -1,15 +1,32 @@
 package com.playtomic.tests.wallet.api.controller;
 
+import com.playtomic.tests.wallet.api.dto.WalletDTO;
+import com.playtomic.tests.wallet.api.service.WalletService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/wallets")
 public class WalletController {
-    private Logger log = LoggerFactory.getLogger(WalletController.class);
+    private final Logger log = LoggerFactory.getLogger(WalletController.class);
+
+    private final WalletService walletService;
+
+    public WalletController(WalletService walletService) {
+        this.walletService = walletService;
+    }
+
+    @GetMapping(value = "/{walletId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<WalletDTO> getWallet(@PathVariable int walletId) {
+        return ResponseEntity.status(HttpStatus.OK).body(walletService.getWallet(walletId));
+    }
 
     @GetMapping("/")
     void log() {
