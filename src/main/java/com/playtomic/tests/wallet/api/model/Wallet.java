a52781f530a52433f94e7f1cc62587c1aaf5ea03
adamu abdulaziz <ajitata6f@gmail.com>
2022-01-03 07:31:53 +0100

    refactor: update id GenerationType

--- src/main/java/com/playtomic/tests/wallet/api/model/Wallet.java
+++ src/main/java/com/playtomic/tests/wallet/api/model/Wallet.java
@@ -17,7 +17,7 @@ import java.util.Objects;
 public class Wallet implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(name = "balance")
