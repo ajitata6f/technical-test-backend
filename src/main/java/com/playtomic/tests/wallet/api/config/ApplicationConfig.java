e7830f53d91b13622e4b46fcbaecaf12bf6cb141
adamu abdulaziz <ajitata6f@gmail.com>
2022-01-05 20:45:17 +0100

    feat: add implementation of taskExecutor bean

--- src/main/java/com/playtomic/tests/wallet/api/config/ApplicationConfig.java
+++ src/main/java/com/playtomic/tests/wallet/api/config/ApplicationConfig.java
@@ -1,16 +1,31 @@
 package com.playtomic.tests.wallet.api.config;
 
-import com.playtomic.tests.wallet.service.StripeService;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
+@EnableAsync
 public class ApplicationConfig {
 
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public ThreadPoolTaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(3);
+        executor.setMaxPoolSize(4);
+        executor.setQueueCapacity(125);
+        executor.setThreadNamePrefix("async-task-thread-");
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.initialize();
+
+        return executor;
     }
 
 }
